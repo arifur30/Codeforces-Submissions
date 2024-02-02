de33c4e1f8d3f1d99d8f1456666618aa2457e181
Arifur_30 <arifafjr17@gmail.com>
2024-02-02 15:46:37 +0600

    "new"

--- A_Raising_Bacteria.cpp
+++ A_Raising_Bacteria.cpp
@@ -13,12 +13,9 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     
-    ll t;
-    cin>>t;
-    if(t%2 == 0)
-        cout<<1<<nl;
-    else
-        cout<<(t%2)+1<<nl;
+    
+    cout<<536870911- pow(2,28)<<nl;
+
     
     return 0;
 }
